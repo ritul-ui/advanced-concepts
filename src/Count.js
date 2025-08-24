d27e4aeda270f1da5788d858cbc8ca5bafc8394f
ritul-ui <ritulanand1996@gmail.com>
2025-08-24 09:34:14 +0530

    transition code added

---
+++ src/Count.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Count({ text, count }) {
+  console.log("rendering count component for " + text);
+  return <div>{`Showing ${text} : ${count}`}</div>;
+}
+
+export default React.memo(Count);
